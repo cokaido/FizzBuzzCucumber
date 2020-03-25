f62f7de14a2d1cd952da748a423f73cb0d57f5ed
Guillem Fernandez <hello@guillem.cat>
2020-03-25 15:54:31 +0100

    Implemented Steps for feature

--- src/test/java/fizzbuzz/StepDefinitions.java
+++ src/test/java/fizzbuzz/StepDefinitions.java
@@ -1,11 +1,65 @@
 package fizzbuzz;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 public class StepDefinitions {
+    private String[] parameters;
+    private String currentAnswer;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStream() {
+        System.setOut(originalOut);
+    }
+
+    @Given("we send no params")
+    public void we_send_no_params() {
+        parameters = new String[]{};
+    }
+
+    @When("we call fizzbuzz app")
+    public void we_call_fizzbuzz_app() {
+        FizzBuzzApp.main(parameters);
+
+        String returned = outContent.toString();
+
+        if (returned.length() == 0) {
+            currentAnswer = "";
+            return;
+        }
+
+        currentAnswer = returned.substring(0, returned.length()-1).replaceAll("\n", ",");
+    }
+
+    @Then("we should see nothing")
+    public void we_should_see_nothing() {
+        assertEquals("", currentAnswer);
+    }
+
+    @Given("we send {string}")
+    public void we_send(String params) {
+        parameters = params.split(" ");
+    }
+
+    @Then("we should see {string}")
+    public void we_should_see(String expectedAnswer) {
+        assertEquals(expectedAnswer, currentAnswer);
+    }
 
 }
